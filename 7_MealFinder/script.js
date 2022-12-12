6d4226127e546e4f87162359ba8d88e3d00e396a
Yukie Kawamoto <arurukun0306@gmail.com>
2022-12-12 17:13:25 +0900

    random

--- 7_MealFinder/script.js
+++ 7_MealFinder/script.js
@@ -7,6 +7,8 @@ const singleMeal=document.getElementById("single-meal");
 
 // https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
 
+// https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}
+
 async function searchMeal(e){
     e.preventDefault();
     
@@ -17,7 +19,7 @@ async function searchMeal(e){
 
     // empty and show arror
     if(searching.trim()){
-        const {data}=await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searching}`)
+        const {data}=await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`)
         
         searchHeading.innerHTML=`<div class="text-1xl mt-5">Search result for "${searching}"</div>`
         
@@ -36,7 +38,7 @@ async function searchMeal(e){
                 </div>
                 `).join('')}
             </div>`
-            console.log(data.meals[0].strMealThumb)
+            // console.log(data.meals[0].strMealThumb)
         }
         
     }else{alert("Please enter a search term")}
@@ -45,9 +47,71 @@ async function searchMeal(e){
 
 submit.addEventListener("submit",searchMeal)
 
-meals.addEventListener("click",e=>{
+meals.addEventListener("click", async(e)=>{
     const mealInfo=e.path[0]
 
-    const mealId=mealInfo.getAttribute("class")
-    console.log(mealId)
+    const mealId=mealInfo.getAttribute("id")
+    const {data}=await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+
+    const meal=data.meals[0]
+
+    let ingredient=[];
+    for(let i=1;i<=20;i++){
+        if(meal[`strIngredient${i}`]){
+            ingredient.push(`${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`)
+        }else{
+            break;
+        }
+    }
+    console.log(ingredient)
+
+
+    meals.innerHTML=`
+    <div class='flex flex-col items-center justify-center'>
+        <P class="m-5 text-xl font-bold">${meal.strMeal}</p>
+        <img src=${meal.strMealThumb} class="w-96 h-96 object-cover"></img>
+        <p class="font-bold mt-2">${meal.strArea ? meal.strArea : ""}</p>
+        <p class="font-bold mt-2">${meal.strCategory ? meal.strCategory : ""}</p>
+        <div class="mx-2 my-12 max-w-[300px]">${meal.strInstructions}</div>
+        <ul class="mb-6">
+            ${ingredient.map(ing=>`<li>${ing}</li>`).join("")}
+        </ul>
+    </div>`
+    console.log(ingredient)
+    console.log(meal)
 })
+
+randomBtn.addEventListener("click",async()=>{
+    meals.innerHTML="";
+
+    const {data}=await axios.get("https://www.themealdb.com/api/json/v1/1/random.php?")
+    const meal=data.meals[0]
+
+    let ingredient=[];
+    for(let i=1;i<=20;i++){
+        if(meal[`strIngredient${i}`]){
+            ingredient.push(`${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`)
+        }else{
+            break;
+        }
+    }
+    console.log(ingredient)
+
+
+    meals.innerHTML=`
+    <div class='flex flex-col items-center justify-center'>
+        <P class="m-5 text-xl font-bold">${meal.strMeal}</p>
+        <img src=${meal.strMealThumb} class="w-96 h-96 object-cover"></img>
+        <p class="font-bold mt-2">${meal.strArea ? meal.strArea : ""}</p>
+        <p class="font-bold mt-2">${meal.strCategory ? meal.strCategory : ""}</p>
+        <div class="mx-2 my-12 max-w-[300px]">${meal.strInstructions}</div>
+        <ul class="mb-6">
+            ${ingredient.map(ing=>`<li>${ing}</li>`).join("")}
+        </ul>
+    </div>`
+    console.log(ingredient)
+    console.log(meal)
+})
+
+
+// await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=su`)
