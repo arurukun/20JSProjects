1fc0a8d0629068507eff39eda43dc8f60f202870
Yukie Kawamoto <arurukun0306@gmail.com>
2022-12-18 00:13:43 +0900

    show score set time

--- 10_GameTyping/script.js
+++ 10_GameTyping/script.js
@@ -9,7 +9,7 @@ const score=document.getElementById("score")
 const time=document.getElementById("time")
 const scoreContainer=document.getElementById("score-container")
 const timeContainer=document.getElementById("time-container")
-const endGame=document.getElementById("end-game-container")
+const endGameContainer=document.getElementById("end-game-container")
 
 const words=[
     "admit",
@@ -21,12 +21,44 @@ const words=[
     "tens"
 ]
 
-let randomword;
+let randomWord;
 let scores=0;
-let times=0;
+let times=10;
+
+// cursor put on at start
+text.focus();
+
+const timeInterval=setInterval(setTime,1000);
 
 function getRandomWord(){
-    return words[Math.floor(Math.random()* words.length)]
+    randomWord=words[Math.floor(Math.random()* words.length)]
+    word.innerText=randomWord
 }
 
 // console.log(getRandomWord())
+
+text.addEventListener("input",e=>{
+    const insertedWord=e.target.value;
+
+    if(insertedWord == randomWord){
+        e.target.value=""
+        console.log(insertedWord)
+        getRandomWord();
+        scores++
+        score.innerText=scores;
+    }
+})
+
+function setTime(){
+    times--;
+    time.innerText=times + "s"
+
+    if(times === 0){
+        clearInterval(timeInterval);
+        const gameOver=()=>{
+
+        }
+    }
+}
+
+getRandomWord()
